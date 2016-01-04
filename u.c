982cda12373d061362432ed27ba61f971712c5e1
Max L <wosis@web.de>
2016-01-05 00:00:47 +0100

    VER 2.33 aum fixed debug maps

--- u.c
+++ u.c
@@ -64,9 +64,10 @@
  * 19.05.2015   2.30e  aum fixed bug in http.c introduced in 2.30a
  * 28.05.2015   2.31   aum bumping version, compressed www.c
  * 29.05.2015   2.32   fx2 new VAR  CPU:USAGE  for web-pages  (=100-IDLE)
+ * 01.01.2016   2.33   aum fixed debug maps
 */
 
-char *cstr = "lg.srv, V2.32 compiled 29.05.2015, by audimax";
+char *cstr = "lg.srv, V2.33 compiled 01.01.2016, by audimax";
 
 int	debug = 0;		// increasing debug output (0 to 9)
 
